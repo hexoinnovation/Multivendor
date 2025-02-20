574c4af4d333ddb810fb9621a72eeca67d39d78e
Nithyavs <hexoinnovation@gmail.com>
2025-02-20 01:02:03 -0800

    Nithya

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -13,7 +13,7 @@ import Contact from "./components/Contact";
 import Faq from "./components/Faq";
 import FlashSale from "./components/FlashSale";
 import FourZeroFour from "./components/FourZeroFour";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 import HomeTwo from "./components/HomeTwo";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import ProductsCompaire from "./components/ProductsCompaire/index";
@@ -23,16 +23,16 @@ import SingleProductPage from "./components/SingleProductPage";
 import TermsCondition from "./components/TermsCondition/index";
 import TrackingOrder from "./components/TrackingOrder";
 import Wishlist from "./components/Wishlist";
-import HomeThree from "./components/HomeThree";
-import HomeFour from "./components/HomeFour";
-import HomeFive from "./components/HomeFive";
+// import HomeThree from "./components/HomeThree";
+// import HomeFour from "./components/HomeFour";
+// import HomeFive from "./components/HomeFive";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  // { path: "/", element: <Home /> },
   { path: "/home-two", element: <HomeTwo /> },
-  { path: "/home-three", element: <HomeThree /> },
-  { path: "/home-four", element: <HomeFour /> },
-  { path: "/home-five", element: <HomeFive /> },
+  // { path: "/home-three", element: <HomeThree /> },
+  // { path: "/home-four", element: <HomeFour /> },
+  // { path: "/home-five", element: <HomeFive /> },
   { path: "/all-products", element: <AllProductPage /> },
   { path: "/single-product", element: <SingleProductPage /> },
   { path: "/cart", element: <CardPage /> },
--- src/components/Partials/Headers/HeaderTwo/Navbar.jsx
+++ src/components/Partials/Headers/HeaderTwo/Navbar.jsx
@@ -685,7 +685,7 @@ export default function Navbar({ className }) {
               <div className="nav">
                 <ul className="nav-wrapper flex xl:space-x-10 space-x-5">
                   <li className="relative">
-                    <Link to="/">
+                    <Link to="/home-two">
                       <span className="flex items-center text-sm text-white font-600 cursor-pointer ">
                         <span>Homepage</span>
                         <span className="ml-1.5 ">
@@ -700,49 +700,7 @@ export default function Navbar({ className }) {
                           boxShadow: "0px 15px 50px 0px rgba(0, 0, 0, 0.14)",
                         }}
                       >
-                        <div className="categories-wrapper w-full h-full p-5">
-                          <div>
-                            <div className="category-items">
-                              <ul className="flex flex-col space-y-2">
-                                <li>
-                                  <Link to="/">
-                                    <span className="text-qgray text-sm font-400 border-b border-transparent hover:text-qyellow hover:border-qyellow">
-                                      Home One
-                                    </span>
-                                  </Link>
-                                </li>
-                                <li>
-                                  <Link to="/home-two">
-                                    <span className="text-qgray text-sm font-400 border-b border-transparent hover:text-qyellow hover:border-qyellow ">
-                                      Home Two
-                                    </span>
-                                  </Link>
-                                </li>
-                                <li>
-                                  <Link to="/home-three">
-                                    <span className="text-qgray text-sm font-400 border-b border-transparent hover:text-qyellow hover:border-qyellow">
-                                      Home Three
-                                    </span>
-                                  </Link>
-                                </li>
-                                <li>
-                                  <Link to="/home-four">
-                                    <span className="text-qgray text-sm font-400 border-b border-transparent hover:text-qyellow hover:border-qyellow">
-                                      Home Four
-                                    </span>
-                                  </Link>
-                                </li>
-                                <li>
-                                  <Link to="/home-five">
-                                    <span className="text-qgray text-sm font-400 border-b border-transparent hover:text-qyellow hover:border-qyellow">
-                                      Home Five
-                                    </span>
-                                  </Link>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
+                        
                       </div>
                     </div>
                   </li>
@@ -771,7 +729,7 @@ export default function Navbar({ className }) {
                             <div className="category-items">
                               <ul className="flex flex-col space-y-2">
                                 <li>
-                                  <a href="#">
+                                  <a href="/all-products">
                                     <span className="text-qgray text-sm font-400 border-b border-transparent hover:border-qyellow hover:text-qyellow">
                                       Shop Sidebar
                                     </span>
